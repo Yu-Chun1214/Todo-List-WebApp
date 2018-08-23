981c7695a36cd9c9e2161844b1b6c9cfe2b62865
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-08-23 10:22:17 +0800

    finish the select part

--- static/back.js
+++ static/back.js
@@ -43,23 +43,56 @@ function list_loadData(data){
 }
 
 function getItemFromServer(load){
-    var redata;
+    
     $.ajax({
         url : API,
         method : 'GET',
         success: function (data){
-            load(data)
+            load(data);
         },
     })
+    
 }
 function delete_loadData(data){
     const dataHtml = choose_generateDataHtml(data);
-    console.log(dataHtml);
-    var outer = `<select class="custom-select" id="inputGroupSelect02" name="delete"></select>`;
-    if(document.querySelector('[name="delete"]')===null){
-        $('#user-input').append(outer);
-    }
-    $('[name="delete"]').append(dataHtml);
+    const inner = `<div id="mode-select">
+    <select class="custom-select" id="inputGroupSelect02">
+      <option selected>Delete</option>
+      <option value="1">Add</option>
+      <option value="2">Edit</option>
+    </select>
+  </div>`;
+  const outter = `<div class="input-group mb-3" id="user-input">`+`</div>`;
+  console.log("delete_loadData #user-input");
+    $('.event').append(outter);
+    $('#user-input').append(inner+dataHtml);
+
+}
+function edit_loadData(data){
+    const data_html = choose_generateDataHtml(data);
+    const inner = `<div id="mode-select">
+    <select class="custom-select" id="inputGroupSelect02">
+    <option selected>Edit</option>
+    <option value="1">Add</option>
+    <option value="2">Delete</option>
+    </select>
+  </div>`+data_html+`<div class="input-group-append" id="input-block">
+  <div class="form-group">
+    <input type="event" class="form-control" id="eventInput" placeholder="Enter event" style="width: 400px;">
+  </div>
+</div>
+<div id="importance-select">
+  <select class="custom-select" id="inputGroupSelect02">                
+      <option selected>Importance</option>
+      <option value="1">Very Important</option>
+      <option value="2">Normal</option>
+      <option value="3">Less Important</option>
+  </select>
+</div>`;
+const outter = `<div class="input-group mb-3" id="user-input"></div>`
+$('.event').append(outter);
+$('#user-input').append(inner);
+console.log('edit_loadData');
 }
 
 $('ul.list-group').delegate('li','click',function(e){
@@ -78,59 +111,89 @@ $('.test2').click(function(e){
     }
 })
 
-$('select').change(function(e){
+$('.event').on("change","select",function(e){
     var mode = $(this).children('option:selected').text();
     console.log(mode);
     if(mode==='Edit'){
-        document.getElementById('input-block').remove();
-        document.getElementById('importance-select').remove();
-        
+        $('#user-input').remove();
+        console.log('else if mode===Edit');
+        getItemFromServer(edit_loadData);
     }
     else if(mode === 'Delete'){
-        document.getElementById('input-block').remove();
-        document.getElementById('importance-select').remove();
+        $('#user-input').remove();
+        console.log('else if mode===Delete');
         getItemFromServer(delete_loadData);
         
     }
     else if(mode==='Add'){
-        console.log(mode)
-        if(document.querySelector('[name="delete"]')!=null){
-            document.querySelector('[name="delete"]').remove();
-        }
-        if(document.getElementById('input-block')===null && document.getElementById('importance-select')===null){
-            $('#user-input').append(`
-            <div class="input-group-append" id="input-block">
-              <div class="form-group">
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" style="width: 800px;">
-              </div>
-            </div>
-            <div id="importance-select">
-            <select class="custom-select" id="inputGroupSelect02">                
-                <option selected>Importance</option>
-                <option value="1">Very Important</option>
-                <option value="2">Normal</option>
-                <option value="3">Less Important</option>
-            </select>
-            </div>
-            `)
-
-        }
+        $('#user-input').remove();
+        $('.event').append(`<div class="input-group mb-3" id="user-input">
+        <div id="mode-select">
+          <select class="custom-select" id="inputGroupSelect02">
+          <option selected>Add</option>
+          <option value="1">Edit</option>
+          <option value="2">Delete</option>
+          </select>
+        </div>
+        
+        <div class="input-group-append" id="input-block">
+          <div class="form-group">
+            <input type="event" class="form-control" id="eventInput" placeholder="Enter event" style="width: 800px;">
+          </div>
+        </div>
+        <div id="importance-select">
+          <select class="custom-select" id="inputGroupSelect02">                
+              <option selected>Importance</option>
+              <option value="1">Very Important</option>
+              <option value="2">Normal</option>
+              <option value="3">Less Important</option>
+          </select>
+        </div>
+      </div>`);
     }
 })
 
 function main_list(){
-    getItemFromServer(list_loadData);
+    var data = getItemFromServer(list_loadData);
 }
 
 function choose_generateDataHtml(data){
-    element_html='';
+    element_html='<select class="custom-select" id="inputGroupSelect02" name="delete">';
     for(item of data){
         var data_html=`
-        <option value="${item['id']}"><h1>"${item['id']} "</h1>${item['event']}</option>
+        <option value="${item['id']}"><h1>${item['id']} </h1>${item['event']}</option>
         `
         element_html += data_html;
     }
-    return element_html;
+    return element_html+'</select>';
 }
-
+function editInput(){
+    var data_html = `
+    <div id="mode-select">
+              <select class="custom-select" id="inputGroupSelect02">
+                <option selected>Mode</option>
+                <option value="1">Add</option>
+                <option value="2">Edit</option>
+                <option value="3">Delete</option>
+              </select>
+            </div>
+    <div class="input-group-append" id="input-block">
+              <div class="form-group">
+                <input type="event" class="form-control" id="eventInput" placeholder="Enter event" style="width: 800px;">
+              </div>
+            </div>
+            <div id="importance-select">
+              <select class="custom-select" id="inputGroupSelect02">                
+                  <option selected>Importance</option>
+                  <option value="1">Very Important</option>
+                  <option value="2">Normal</option>
+                  <option value="3">Less Important</option>
+              </select>
+            </div>
+        </div>
+    `
+  if(document.getElementById('input-block')===null || document.getElementById('importance-select')===null){
+      $('#user-input').html(data_html);
+  }
+}
 main_list();
